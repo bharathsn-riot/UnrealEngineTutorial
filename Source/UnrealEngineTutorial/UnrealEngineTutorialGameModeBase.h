38b8184ae0195b99901f0a44cc6bb7b57b65d19f
Bharath S Nagappa <bnagappa@riotgames.com>
2023-06-02 20:16:21 -0700

    Unreal tutorial first commit

---
+++ Source/UnrealEngineTutorial/UnrealEngineTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealEngineTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALENGINETUTORIAL_API AUnrealEngineTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
